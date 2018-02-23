46d51ee2f6891d0a7556ade476cd5f562e19b551
mduby <mduby@broadinstitute.org>
2018-02-22 21:03:19 -0500

    added new access

--- grails-app/controllers/org/broadinstitute/variantgrade/HeatMapController.groovy
+++ grails-app/controllers/org/broadinstitute/variantgrade/HeatMapController.groovy
@@ -7,7 +7,7 @@ class HeatMapController {
     HeatMapService heatMapService
 
     // instance variables
-    private String passcode = "broadvariantfunction";
+    private String passcode = "broaddeviationstandard";
 
     def index() { }
 
